01b9878be71eeb4f8d89a06513d93eb6d721a4a5
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2022-12-01 10:47:33 +0100

    feat: add webpack.config (#10)
    * feat: add modules for vue and webpack
    
    * fix: nativescript webpack file
    
    * fix: build ns app
    
    * fix: package repository
    
    * Fix peer dependency
    
    Co-authored-by: Yukino Song <yukino@yuki.network>

---
+++ nativescript.webpack.js
@@ -0,0 +1,71 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
+const dominativeElements = [
+    'AbsoluteLayout',
+    'ActionBar',
+    'ActionItem',
+    'ActivityIndicator',
+    'Button',
+    'ContentView',
+    'DatePicker',
+    'DockLayout',
+    'FlexboxLayout',
+    'FormattedString',
+    'Frame',
+    'GridLayout',
+    'HtmlView',
+    'Image',
+    'Label',
+    'ListPicker',
+    'ListView',
+    'NavigationButton',
+    'Page',
+    'Placeholder',
+    'Progress',
+    'ProxyViewContainer',
+    'RootLayout',
+    'ScrollView',
+    'SearchBar',
+    'SegmentedBar',
+    'SegmentedBarItem',
+    'Slider',
+    'Span',
+    'StackLayout',
+    'Switch',
+    'TabView',
+    'TabViewItem',
+    'TextField',
+    'TextView',
+    'TimePicker',
+    'WebView',
+    'WrapLayout',
+    'Prop',
+    'Template',
+];
+
+// DOC: https://docs.nativescript.org/webpack.html#plugin-api
+module.exports = webpack => {
+    webpack.chainWebpack((config, env) => {
+        config.resolve.alias.set('@vue/runtime-dom', '@vue/runtime-dom');
+        config.resolve.alias.set('vue', '@vue/runtime-dom');
+        config.plugin('VueLoaderPlugin').use(VueLoaderPlugin);
+        config.module
+            .rule('vue')
+            .test(/\.vue$/)
+            .use('vue-loader')
+            .loader(require.resolve('vue-loader'))
+            .tap((options) => {
+                return {
+                    ...options,
+                    isServerBuild: false,
+                    compilerOptions: {
+                        isCustomElement: (tag) => dominativeElements.indexOf(tag) >= 0,
+                        hoistStatic: false,
+                    },
+                };
+            });
+    });
+}
+
+
+
